394b12e053088838640963a090582aadf7222148
ngr-ch <111493473+ngr-ch@users.noreply.github.com>
2022-10-21 16:08:47 +0530

    Update Fetch_Word_with_intial_chars from a Big File.py

--- Fetch_Word_with_intial_chars from a Big File.py
+++ Fetch_Word_with_intial_chars from a Big File.py
@@ -3,7 +3,7 @@ import sys
 import re
   
 
-inpu_file = open(r"C:\temp\PyTest\Castor_variables_04Aug2022.csv", "r").read()
+inpu_file = open(r"C:\temp\PyTest\variables_04Aug2022.csv", "r").read()
 
 for line in re.findall("srv.....", inpu_file):
     print(line)
